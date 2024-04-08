39b44e18fb08bc5d6a356ea73edcc86ed3f4dc70
not3amer <not3amer2003@gmail.com>
2024-04-08 15:50:37 +0200

    Add New Assignments Soul

---
+++ 03- Elzero JS Course Soultions/04-Assignments From 023 to 026/Assignment 01/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(+"100000"); // 100000
+console.log(100000.00); // 100000
+console.log(1e5); // 100000
+console.log(10 * 10000); // 100000
+console.log(200000 / 2); // 100000
+console.log(2e5 - 1e5); // 100000
+console.log(-(-100000)); // 100000
+console.log(Math.min(1e5)); // 100000
+console.log(1_00_000); // 100000
+console.log(Math.trunc(1e5)); // 100000
