aed3bbfe57e4d18cc6bc974ea25b6ffe9bce8b8c
not3amer <not3amer2003@gmail.com>
2024-04-08 12:34:07 +0200

    Add Elzero JS Course Soultions From 001 to 022

---
+++ 03- Elzero JS Course Soultions/01-Assignments From 001 to 009/Assignment 06/main.js
@@ -0,0 +1,11 @@
+//First Method:
+
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
+
+//Second Method:
+anywordstogeterror
+console.log("Iam In Console");
+document.write("Iam In Page");
